1dac6b1e6ee5240366c40e7dafbeb09bb8674344
Waleed Javed <waleedjavied@gmail.com>
2023-10-06 20:48:10 +0500

    ticker added and responsive for sm and xsm breakpoints

--- src/app/components/ticker-marquee/ticker-marquee.component.ts
+++ src/app/components/ticker-marquee/ticker-marquee.component.ts
@@ -1,6 +1,7 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core';
 import {gsap} from 'gsap';
+import { window } from 'rxjs';
 
 @Component({
   selector: 'app-ticker-marquee',
@@ -9,6 +10,7 @@ import {gsap} from 'gsap';
 })
 export class TickerMarqueeComponent implements OnInit{
 
+  
   constructor( @Inject(DOCUMENT) private documnt:Document){}
   
   ngOnInit(): void {
@@ -16,6 +18,30 @@ export class TickerMarqueeComponent implements OnInit{
   }
 
   animate(){
+    console.log("Animation Started");
+
+    let firstItem= this.documnt.querySelectorAll(".first"); 
+    let secondItem= this.documnt.querySelectorAll(".second"); 
+    let star = this.documnt.querySelectorAll(".marquee__star")
+    
+    let tl = gsap.timeline();
+
+    gsap.fromTo(star,{rotation:0},{rotation:360, duration:1.5, ease:"power4.inOut", scale:1.5 , repeat:-1});
+    
+    tl.fromTo(firstItem,{x:0},{
+      x:-2400,
+      duration:12.5,
+      ease:"linear",
+    });
+
+    tl.fromTo(secondItem,{x:0},{
+      x:-2400,
+      duration:12.5,
+      ease:"linear",
+      delay:-12.5,
+    });
+
+    tl.repeat(Infinity);
   }
 
 }

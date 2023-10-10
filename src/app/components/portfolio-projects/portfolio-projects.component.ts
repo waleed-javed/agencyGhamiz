2eead745a6ca7a714dba989026adcaacbe00aa45
Waleed Javed <waleedjavied@gmail.com>
2023-10-10 17:41:55 +0500

    cleaned portfolio animation

--- src/app/components/portfolio-projects/portfolio-projects.component.ts
+++ src/app/components/portfolio-projects/portfolio-projects.component.ts
@@ -17,61 +17,41 @@ export class PortfolioProjectsComponent {
 
     this.animate();
   }
-
+  
   animate(){
-
-    const projectContainer:any = gsap.utils.toArray(".project-item:not(:first-child)"); 
-    // get all images
-    let projectDemoImagesExceptFirst:any = gsap.utils.toArray(".project-demo:not(:first-child)");
-    const projectDemoImages:any = gsap.utils.toArray(".project-demo");
     
-    // set images off screen verticaly
-    gsap.set(projectDemoImagesExceptFirst,{yPercent:201,opacity:0.5});
+    let mm = gsap.matchMedia();
 
-    // animate them as scroll happens
-    
-    // const animateImages = gsap.to(projectDemoImagesExceptFirst,{
-    //   yPercent:0, duration:1.5, stagger:1.5
-    // });
-
-    
+    //add a media query. When it matches, the associated function will run
+    mm.add(`(min-width: 1024px)`, () => {
+      
+      const projectContainer:any = gsap.utils.toArray(".project-item:not(:first-child)"); 
+      // get all images
+      const projectDemoImagesExceptFirst:any = gsap.utils.toArray(".project-demo:not(:first-child)");
+      const projectDemoImages:any = gsap.utils.toArray(".project-demo");
+      
+      // set images off screen verticaly
+      gsap.set(projectDemoImagesExceptFirst,{yPercent:201,opacity:0.5});
+          
+      ScrollTrigger.create({
+        trigger:".project-gallery",
+        start:"top top",
+        end:"bottom bottom",
+        pin:".projects-images-container",
+      })
+        
+      // dunamically add trigger to content
+      projectContainer.forEach((project: { querySelector: (arg0: string) => any; },index: string | number)=>{
+        let animation = gsap.timeline().to(projectDemoImagesExceptFirst[index], {yPercent:0,opacity:1}) .set(projectDemoImages[index], {autoAlpha:0})
         ScrollTrigger.create({
-          trigger:".project-gallery",
-          start:"top top",
-          end:"bottom bottom",
-          pin:".projects-images-container",
-        })
-
-    
-        // dunamically add trigger to content
-        // let tl= gsap.timeline();
-        projectContainer.forEach((project: { querySelector: (arg0: string) => any; },index: string | number)=>{
-
-          // let headline = project.querySelector(".project-stats")
-          // let headline = project.querySelector("h1")
-          let animation = gsap.timeline().to(projectDemoImagesExceptFirst[index], {yPercent:0,opacity:1}) .set(projectDemoImages[index], {autoAlpha:0})
-
-          ScrollTrigger.create({
             trigger: project.querySelector("h1"),
             start:"top 70%",
             end:"top 40%",
             animation:animation,
             scrub:true,
-            // markers:true
-          })
-
-          return () => { // optional
-            // custom cleanup code here (runs when it STOPS matching)
-            console.log("mobile")
-          };
-        });
-
-
-    //let mm = gsap.matchMedia();
-    //add a media query. When it matches, the associated function will run
-    //mm.add("(min-width: 600px)", () => {});
-
-
+        })
+        return () => {};
+      });
+    });
   }
-    
 }
